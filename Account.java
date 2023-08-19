339cda26d843009ae85e8977ea04442bdfb49793
bishavro1 <142042820+bishavro1@users.noreply.github.com>
2023-08-19 18:04:54 +0600

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,60 +1,46 @@
-public class Account
-{
+public class Account {
     private String name;
     private double balance;
-    private String account;
+    private String accountType;
     
-    public Account(String name, String balance, String account)
-    {
+    public Account(String name, double balance, String accountType) {
         this.name = name;
-        double initialBalance = Double.parseDouble(balance);
-        if (initialBalance > 0.0)
-        {
-            this.balance = initialBalance;
+        
+        if (balance > 0.0) {
+            this.balance = balance;
         }
-        this.account = account;
+        
+        this.accountType = accountType;
     }
     
-    public void deposit(double depositAmount)
-    {
-        if (depositAmount > 0.0)
-        {
+    public void deposit(double depositAmount) {
+        if (depositAmount > 0.0) {
             balance = balance + depositAmount;
         }
     }
     
-    public double getBalance()
-    {
+    public double getBalance() {
         return balance;
     }
     
-    public void setName(String name)
-    {
+    public void setName(String name) {
         this.name = name;
     }
     
-    public String getName()
-    {
+    public String getName() {
         return name;
     }
     
-    public void setAccount(String account)
-    {
-        this.account = account;
+    public String getAccountType() { // Changed the method name to be more descriptive
+        return accountType;
     }
     
-    public String getAccount()
-    {
-        return account;
-    }
-    
-    public static void main(String[] args)
-    {
-        Account myAccount = new Account("Avro Deb", "2500.0", "Personal");
+    public static void main(String[] args) {
+        Account myAccount = new Account("Avro Deb", 2500.0, "Personal");
         
         // Printing initial account information
         System.out.println("Account Holder: " + myAccount.getName());
-        System.out.println("Account Type: " + myAccount.getAccount());
+        System.out.println("Account Type: " + myAccount.getAccountType());
         System.out.println("Initial Balance: $" + myAccount.getBalance());
         
         myAccount.deposit(2500.0);
